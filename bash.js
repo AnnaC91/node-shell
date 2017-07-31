b24b02d2d4f4f8c2c978429f9fc7ad1fdd3b74e6
Michelle Scharfstein <michelle.scharfstein@gmail.com>
2017-07-31 12:37:02 -0400

    updated files

--- bash.js
+++ bash.js
@@ -1,11 +1,17 @@
+var chalk = require('chalk');
+
 // Output a prompt
-process.stdout.write('prompt > ');
+process.stdout.write(chalk.yellow('prompt') + ' > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-    var cmd = data.toString().trim(); // remove the newline
-    var commands = require("./command")
+    var cmd = data.toString().trim().split(" "); // remove the newline
+    //console.log(cmd[0]);
+    var func = cmd[0];
+    var inputVar = cmd.slice(1);
 
-    commands[cmd]();
+    var commands = require("./command")
+    if (inputVar) commands[func](inputVar);
+    else commands[func]();
 
 });
--- command.js
+++ command.js
@@ -1,19 +1,73 @@
+var chalk = require('chalk');
+
+var fs = require('fs');
+
 module.exports = {
     pwd: function(){
-        process.stdout.write(process.cwd());
-        process.stdout.write('\nprompt > ');
+        process.stdout.write(chalk.green(process.cwd()));
+        process.stdout.write('\n' + chalk.yellow('prompt') + ' > ');
     },
 
     date: function(){
         var d = new Date();
-        process.stdout.write(d.toString());
-        process.stdout.write('\nprompt > ');
+        process.stdout.write(chalk.green(d.toString()));
+        process.stdout.write('\n' + chalk.yellow('prompt') + ' > ');
     },
 
     ls: function(){
-        var fs = require('fs');
-        var files = fs.readdirSync('.');
-        process.stdout.write(files);
-        process.stdout.write('\nprompt > ');
+        fs.readdir('.',function(err,data) {
+            if (err) process.stdout.write(err);
+            else {
+                process.stdout.write(chalk.green(data.toString()));
+                process.stdout.write('\n' + chalk.yellow('prompt') + ' > ');
+            }
+        });
+    },
+
+    echo: function(input) {
+        if (input.toString() === '$PATH') {
+            process.stdout.write(chalk.green(process.env['PATH']));
+        }
+        else {
+            process.stdout.write(chalk.green(input.join(" ")));
+        }
+        process.stdout.write('\n' + chalk.yellow('prompt') + ' > ');
+    },
+
+    cat: function(input) {
+        fs.readFile(input.toString(), function(err,data) {
+            if (err) process.stdout.write(err);
+            else process.stdout.write(chalk.green(data.toString()));
+        });
+    },
+
+    head: function(input) {
+        fs.readFile(input.toString(), function(err,data) {
+            if (err) {
+                process.stdout.write(err);
+            }
+            else {
+                var file = data.toString().split("\n").slice(0,10);
+
+                process.stdout.write(chalk.green(file.join("\n")));
+                process.stdout.write('\n' + chalk.yellow('prompt') + ' > ');
+            }
+        });
+    },
+
+    tail: function(input) {
+        fs.readFile(input.toString(), function(err,data) {
+            if (err) {
+                process.stdout.write(err);
+            }
+            else {
+                var file = data.toString().split("\n");
+                var length = file.length;
+                var abbrevFile = file.slice(file.length-10);
+
+                process.stdout.write(chalk.green(abbrevFile.join("\n")));
+                process.stdout.write('\n' + chalk.yellow('prompt') + ' > ');
+            }
+        });
     }
 }
